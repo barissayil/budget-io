0d9800443f6ae9f8477c31e4bc42a7dda6c114df
Baris Sayil <baris.s@padoa-group.com>
2022-11-05 02:33:10 +0300

    ✨ Adding spendings

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,31 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { findDOMNode } from 'react-dom';
+import { useState } from "react";
 
-const spendingList = [
-  {
-    id: 0,
-    date: new Date(),
-    amount: 10,
-    category: 'food',
-  },
-  {
-    id: 1,
-    date: new Date(),
-    amount: 5,
-    category: 'health',
-  },
-  {
-    id: 2,
-    date: new Date(),
-    amount: 1000,
-    category: 'rent',
-  },
-];
+enum SpendingCategory {
+  Housing = 'HOUSING',
+  Utilities = 'UTILITIES',
+  Food = 'FOOD',
+  Health = 'HEALTH',
+  Transport = 'TRANSPORT',
+  Fun = 'FUN',
+  Important = 'IMPORTANT',
+  Wasted = 'WASTED',
+}
+
+interface Spending {
+  id: number,
+  date: string,
+  amount: number,
+  category: SpendingCategory,
+}
 
 const Home: NextPage = () => {
+  const [spendingList, setSpendingList] = useState<Spending[]>([]);
+  const [date, setDate] = useState<string>(new Date().toISOString().slice(0, 10));
+  const [amount, setAmount] = useState<number>(0);
+  const [category, setCategory] = useState<SpendingCategory>(SpendingCategory.Housing);
   return (
     <div>
       <Head>
@@ -35,12 +37,44 @@ const Home: NextPage = () => {
       <main>
         <ul>
           {
-            spendingList.map((spending) => <li key={spending.id}>{`${spending.date.toLocaleDateString()}: ${spending.amount}€ on ${spending.category}`}</li>)
+            spendingList.map((spending) => <li key={spending.id}>{`${spending.date}: ${spending.amount}€ - ${spending.category}`}</li>)
           }
         </ul>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            const currentSpending: Spending = {
+              id: spendingList.length,
+              date,
+              amount,
+              category,
+            }
+            setSpendingList([...spendingList, currentSpending])
+          }}>
+          <label>
+              Date: <input name="date" type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
+          </label>
+          <br />
+          <label>
+              Amount: <input name="amount" type="number" value={amount} onChange={(e) => setAmount(+e.target.value)}></input>
+          </label>
+          <br />
+          <label>Category: </label>
+          <select name="category" value={category} onChange={(e) => setCategory(e.target.value as SpendingCategory)}>
+            <option value={SpendingCategory.Housing}>Housing</option>
+            <option value={SpendingCategory.Utilities}>Utilities</option>
+            <option value={SpendingCategory.Food}>Food</option>
+            <option value={SpendingCategory.Health}>Health</option>
+            <option value={SpendingCategory.Transport}>Transport</option>
+            <option value={SpendingCategory.Fun}>Fun</option>
+            <option value={SpendingCategory.Important}>Important</option>
+            <option value={SpendingCategory.Wasted}>Wasted</option>
+          </select>
+          <br />
+          <input type="submit" value="Add spending" />
+        </form>
       </main>
     </div>
   )
 }
 
-export default Home
+export default Home;
