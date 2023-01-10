c9b5877209f29db16e1ac1b5de65081a875e0334
Baris Sayil <barissayil@protonmail.com>
2023-01-10 22:07:05 +0100

    ♻️ Layout component

---
+++ components/layout.tsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+
+enum SpendingCategory {
+  Housing = 'HOUSING',
+  Utilities = 'UTILITIES',
+  Food = 'FOOD',
+  Health = 'HEALTH',
+  Transport = 'TRANSPORT',
+  Fun = 'FUN',
+  Important = 'IMPORTANT',
+  Wasted = 'WASTED',
+}
+
+type Spending = {
+  id: number,
+  date: string,
+  amount: number,
+  category: SpendingCategory,
+}
+
+const Layout = () => {
+  const [spendingList, setSpendingList] = useState<Spending[]>([]);
+  useEffect(()=> {
+    const stringifiedSpendingListFromLocalStorage = localStorage.getItem('spendingList');
+    if (!stringifiedSpendingListFromLocalStorage) return;
+    const spendingListFromLocalStorage = JSON.parse(stringifiedSpendingListFromLocalStorage);
+    setSpendingList(spendingListFromLocalStorage);
+  }, [])
+  const [date, setDate] = useState<string>(new Date().toISOString().slice(0, 10));
+  const [amount, setAmount] = useState<number>(0);
+  const [category, setCategory] = useState<SpendingCategory>(SpendingCategory.Housing);
+  return (
+    <main>
+      <ul>
+        {
+          spendingList.map((spending) => <li key={spending.id}>{`${spending.date}: ${spending.amount}€ - ${spending.category}`}</li>)
+        }
+      </ul>
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          const currentSpending: Spending = {
+            id: spendingList.length,
+            date,
+            amount,
+            category,
+          };
+          const newSpendingList = [...spendingList, currentSpending];
+          setSpendingList(newSpendingList);
+          const stringifiedNewSpendingList = JSON.stringify(newSpendingList);
+          localStorage.setItem('spendingList', stringifiedNewSpendingList);
+        }}>
+        <label>
+            Date: <input name="date" type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
+        </label>
+        <br />
+        <label>
+            Amount: <input name="amount" type="number" value={amount} onChange={(e) => setAmount(+e.target.value)}></input>
+        </label>
+        <br />
+        <label>Category: </label>
+        <select name="category" value={category} onChange={(e) => setCategory(e.target.value as SpendingCategory)}>
+          <option value={SpendingCategory.Housing}>Housing</option>
+          <option value={SpendingCategory.Utilities}>Utilities</option>
+          <option value={SpendingCategory.Food}>Food</option>
+          <option value={SpendingCategory.Health}>Health</option>
+          <option value={SpendingCategory.Transport}>Transport</option>
+          <option value={SpendingCategory.Fun}>Fun</option>
+          <option value={SpendingCategory.Important}>Important</option>
+          <option value={SpendingCategory.Wasted}>Wasted</option>
+        </select>
+        <br />
+        <input type="submit" value="Add" />
+      </form>
+      <button onClick={() => {
+        setSpendingList([]);
+        localStorage.removeItem('spendingList');
+      }}>
+        Clear
+      </button>
+    </main>
+  );
+}
+ 
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,90 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { findDOMNode } from 'react-dom';
-import { useEffect, useState } from "react";
 import Meta from '../components/meta';
-
-enum SpendingCategory {
-  Housing = 'HOUSING',
-  Utilities = 'UTILITIES',
-  Food = 'FOOD',
-  Health = 'HEALTH',
-  Transport = 'TRANSPORT',
-  Fun = 'FUN',
-  Important = 'IMPORTANT',
-  Wasted = 'WASTED',
-}
-
-type Spending = {
-  id: number,
-  date: string,
-  amount: number,
-  category: SpendingCategory,
-}
+import Layout from '../components/layout';
 
 const Home: NextPage = () => {
-  const [spendingList, setSpendingList] = useState<Spending[]>([]);
-  useEffect(()=> {
-    const stringifiedSpendingListFromLocalStorage = localStorage.getItem('spendingList');
-    if (!stringifiedSpendingListFromLocalStorage) return;
-    const spendingListFromLocalStorage = JSON.parse(stringifiedSpendingListFromLocalStorage);
-    setSpendingList(spendingListFromLocalStorage);
-  }, [])
-  const [date, setDate] = useState<string>(new Date().toISOString().slice(0, 10));
-  const [amount, setAmount] = useState<number>(0);
-  const [category, setCategory] = useState<SpendingCategory>(SpendingCategory.Housing);
   return (
     <>
       <Meta />
-      <main>
-        <ul>
-          {
-            spendingList.map((spending) => <li key={spending.id}>{`${spending.date}: ${spending.amount}€ - ${spending.category}`}</li>)
-          }
-        </ul>
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            const currentSpending: Spending = {
-              id: spendingList.length,
-              date,
-              amount,
-              category,
-            };
-            const newSpendingList = [...spendingList, currentSpending];
-            setSpendingList(newSpendingList);
-            const stringifiedNewSpendingList = JSON.stringify(newSpendingList);
-            localStorage.setItem('spendingList', stringifiedNewSpendingList);
-          }}>
-          <label>
-              Date: <input name="date" type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
-          </label>
-          <br />
-          <label>
-              Amount: <input name="amount" type="number" value={amount} onChange={(e) => setAmount(+e.target.value)}></input>
-          </label>
-          <br />
-          <label>Category: </label>
-          <select name="category" value={category} onChange={(e) => setCategory(e.target.value as SpendingCategory)}>
-            <option value={SpendingCategory.Housing}>Housing</option>
-            <option value={SpendingCategory.Utilities}>Utilities</option>
-            <option value={SpendingCategory.Food}>Food</option>
-            <option value={SpendingCategory.Health}>Health</option>
-            <option value={SpendingCategory.Transport}>Transport</option>
-            <option value={SpendingCategory.Fun}>Fun</option>
-            <option value={SpendingCategory.Important}>Important</option>
-            <option value={SpendingCategory.Wasted}>Wasted</option>
-          </select>
-          <br />
-          <input type="submit" value="Add" />
-        </form>
-        <button onClick={() => {
-          setSpendingList([]);
-          localStorage.removeItem('spendingList');
-        }}>
-          Clear
-        </button>
-      </main>
+      <Layout />
     </>
   )
 }
