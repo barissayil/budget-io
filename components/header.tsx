136616f6485f4093bcf7021a97a467a87f47236d
Baris Sayil <barissayil@protonmail.com>
2023-01-30 09:07:00 +0100

    ♻️ header

---
+++ components/header.tsx
@@ -0,0 +1,161 @@
+import {
+  createStyles,
+  Header as MantineHeader,
+  HoverCard,
+  Group,
+  Button,
+  UnstyledButton,
+  Text,
+  Anchor,
+  Divider,
+  Center,
+  Box,
+  Burger,
+  Drawer,
+  ScrollArea,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { ReportMoney } from "tabler-icons-react";
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: "flex",
+    alignItems: "center",
+    height: "100%",
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: "none",
+    color: theme.black,
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan("sm")]: {
+      height: 42,
+      display: "flex",
+      alignItems: "center",
+      width: "100%",
+    },
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colors.gray[0],
+    }),
+  },
+
+  subLink: {
+    width: "100%",
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    borderRadius: theme.radius.md,
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colors.gray[0],
+    }),
+
+    "&:active": theme.activeStyles,
+  },
+
+  dropdownFooter: {
+    backgroundColor: theme.colors.gray[0],
+    margin: -theme.spacing.md,
+    marginTop: theme.spacing.sm,
+    padding: `${theme.spacing.md}px ${theme.spacing.md * 2}px`,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${theme.colors.gray[1]}`,
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+const Header = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const { classes, theme } = useStyles();
+
+  return (
+    <Box>
+      <MantineHeader height={60} px="md">
+        <Group position="apart" sx={{ height: "100%" }}>
+          <ReportMoney size={30} />
+          <Text fz="xl" fw={500}>
+            Budget IO
+          </Text>
+
+          <Group
+            sx={{ height: "100%" }}
+            spacing={0}
+            className={classes.hiddenMobile}
+          >
+            <a href="#" className={classes.link}>
+              Spendings
+            </a>
+            <a href="#" className={classes.link}>
+              Reports
+            </a>
+            <a href="#" className={classes.link}>
+              Insights
+            </a>
+            <a href="#" className={classes.link}>
+              Settings
+            </a>
+          </Group>
+
+          <Group className={classes.hiddenMobile}>
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            className={classes.hiddenDesktop}
+          />
+        </Group>
+      </MantineHeader>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
+          <Divider my="sm" color={"gray.1"} />
+
+          <a href="#" className={classes.link}>
+            Spendings
+          </a>
+          <a href="#" className={classes.link}>
+            Reports
+          </a>
+          <a href="#" className={classes.link}>
+            Insights
+          </a>
+          <a href="#" className={classes.link}>
+            Settings
+          </a>
+
+          <Divider my="sm" color={"gray.1"} />
+
+          <Group position="center" grow pb="xl" px="md">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Header;
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import ModifiableSpendingTable from "@components/modifiable-spending-table";
 import { Spending } from "@prisma/client";
+import Header from "@components/header";
 
 type Props = {
   spendings: Spending[];
@@ -8,6 +9,7 @@ type Props = {
 const Layout = ({ spendings }: Props) => {
   return (
     <main>
+      <Header />
       <ModifiableSpendingTable initialSpendings={spendings} />
     </main>
   );
