14c36461d43777b797a9d7e8aa94441bbc29cc89
Baris Sayil <barissayil@protonmail.com>
2023-02-01 18:09:11 +0100

    ✨ edit/delete spendings with auth

--- pages/api/spending/[id].ts
+++ pages/api/spending/[id].ts
@@ -1,6 +1,8 @@
 import { NextApiResponse, NextApiRequest } from "next";
 import { prisma } from "@db/prisma";
 import { Spending } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 type Body = {
   date: string;
@@ -8,32 +10,49 @@ type Body = {
   category: string;
 };
 
-const handleDelete = async (id: string, res: NextApiResponse<Spending>) => {
+const handleDelete = async (id: string, userEmail: string, res: NextApiResponse<Spending>) => {
   const spending = await prisma.spending.delete({
-    where: { id },
+    where: {
+      id_userEmail: {
+        id,
+        userEmail,
+      },
+    },
   });
   res.json(spending);
 };
 
 const handlePut = async (
   id: string,
+  userEmail: string,
   { date, amount, category }: Body,
   res: NextApiResponse<Spending>
 ) => {
   const spending = await prisma.spending.update({
-    where: { id },
+    where: {
+      id_userEmail: {
+        id,
+        userEmail,
+      },
+    },
     data: { date, amount, category },
   });
   res.json(spending);
 };
 
 const handle = async (req: NextApiRequest, res: NextApiResponse<Spending>) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) throw new Error(`Unauthorized.`);
+
+  const userEmail = session.user?.email;
+  if (!userEmail) throw new Error(`User does not exist or has no email.`);
+
   switch (req.method) {
     case "DELETE":
-      await handleDelete(req.query.id as string, res);
+      await handleDelete(req.query.id as string, userEmail, res);
       break;
     case "PUT":
-      await handlePut(req.query.id as string, req.body, res);
+      await handlePut(req.query.id as string, userEmail, req.body, res);
       break;
     default:
       throw new Error(`The HTTP ${req.method} method is not supported at this route.`);
