1c20474a52408af0b6ed582d6e8d6927ed931d20
Baris Sayil <barissayil@protonmail.com>
2023-02-01 17:41:17 +0100

    ✨ get spendings with auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,37 @@ import Meta from "@components/meta";
 import Layout from "@components/layout";
 import { prisma } from "@db/prisma";
 import { Spending } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 type Props = {
   spendings: Spending[];
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: '/',
+  //       permanent: false,
+  //     },
+  //   }
+  // }
+
+  if (!session?.user?.email) {
+    return {
+      props: { spendings: [] },
+    };
+  }
+
   const spendings = await prisma.spending.findMany({
+    where: {
+      user: {
+        email: session.user?.email,
+      },
+    },
     orderBy: {
       date: "asc",
     },
