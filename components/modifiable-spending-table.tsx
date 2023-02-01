22c9cdafffd8923ab7e37389dab4f29cf7a3b267
Baris Sayil <barissayil@protonmail.com>
2023-02-01 15:01:33 +0100

    ✨ custom alert if signed in or not

--- components/modifiable-spending-table.tsx
+++ components/modifiable-spending-table.tsx
@@ -7,6 +7,7 @@ import EditSpendingModal from "@components/modals/edit-spending-modal";
 import AddSpendingModal from "@components/modals/add-spending-modal";
 import DeleteSpendingModal from "@components/modals/delete-spending-modal";
 import { MoodSad } from "tabler-icons-react";
+import { useSession } from "next-auth/react";
 
 type Props = {
   initialSpendings: Spending[];
@@ -18,6 +19,8 @@ const ModifiableSpendingTable = ({ initialSpendings }: Props) => {
   const [openedSpendingModal, setOpenedSpendingModal] = useState<OpenedSpendingModal>(null);
 
   const [selectedSpendingId, setSelectedSpendingId] = useState<string | null>(null);
+
+  const { data: session, status } = useSession();
 
   const getSpending = (id: string): Spending => {
     return spendings.find((spending) => spending.id === id) as Spending;
@@ -61,23 +64,36 @@ const ModifiableSpendingTable = ({ initialSpendings }: Props) => {
         />
       )}
       <div className="flex flex-col flex-auto justify-between items-center gap-2 p-2 bg-slate-200">
-        {spendings.length > 0 ? (
-          <div>
-            <SpendingTable
-              spendings={spendings}
-              openEditSpendingModal={openEditSpendingModal}
-              openDeleteSpendingModal={openDeleteSpendingModal}
-            />
-          </div>
-        ) : (
+        {session ? (
           <>
-            <Alert title="No spendings">There are no spendings for the selected time period.</Alert>
-            <MoodSad size={48} strokeWidth={2} color={"black"} />
+            <Alert title="Hi" color="blue">
+              Hello {session?.user?.name ?? ""}!
+            </Alert>
+            {spendings.length > 0 ? (
+              <div>
+                <SpendingTable
+                  spendings={spendings}
+                  openEditSpendingModal={openEditSpendingModal}
+                  openDeleteSpendingModal={openDeleteSpendingModal}
+                />
+              </div>
+            ) : (
+              <>
+                <Alert title="No spendings" color="yellow">
+                  There are no spendings for the selected time period.
+                </Alert>
+                <MoodSad size={48} strokeWidth={2} color={"black"} />
+              </>
+            )}
+            <Button onClick={() => setOpenedSpendingModal("ADD")} color="cyan">
+              Add spending
+            </Button>
           </>
+        ) : (
+          <Alert title="Sign in" color="yellow">
+            Please sign in
+          </Alert>
         )}
-        <Button onClick={() => setOpenedSpendingModal("ADD")} color="cyan">
-          Add spending
-        </Button>
       </div>
     </>
   );
