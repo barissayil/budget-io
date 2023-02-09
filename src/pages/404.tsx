cbe07f216282fc680de39bdb36c27fc74b7f4d8a
Baris Sayil <barissayil@protonmail.com>
2023-02-09 10:49:54 +0100

    ✨ add blank 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Layout from "@components/layout";
+
+const FourOhFour: NextPage = () => {
+  return (
+    <>
+      <Layout>404</Layout>
+    </>
+  );
+};
+
+export default FourOhFour;
