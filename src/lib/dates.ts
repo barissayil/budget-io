00a9a3cc02492a3318fb97e608e9cd2b8daf6788
Baris Sayil <barissayil@protonmail.com>
2023-02-12 11:41:49 +0100

    ♻️ use day.js

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,17 +1,17 @@
+import dayjs from "dayjs";
+
 export const convertDateObjectToDate = (dateObject: Date): string => {
-  return new Date(dateObject.getTime() - dateObject.getTimezoneOffset() * 60000)
-    .toISOString()
-    .substring(0, 10);
+  return dayjs(dateObject).format().substring(0, 10);
 };
 
 export const getToday = (): string => {
-  return convertDateObjectToDate(new Date());
+  return dayjs().format().substring(0, 10);
 };
 
 export const getCurrentYearMonth = (): string => {
-  return getToday().substring(0, 7);
+  return dayjs().format().substring(0, 7);
 };
 
 export const getCurrentYear = (): string => {
-  return getCurrentYearMonth().substring(0, 4);
+  return dayjs().format().substring(0, 4);
 };
