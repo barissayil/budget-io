5f51d24760e21f353503e08263ee899fdd4a7ef6
Baris Sayil <barissayil@protonmail.com>
2023-02-15 23:49:39 +0100

    ✨ signin page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,6 +16,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+  },
   // debug: true,
 };
 
---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,56 @@
+import { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@api/auth/[...nextauth]";
+import { signIn } from "next-auth/react";
+import Layout from "@components/layout";
+import { Button, Group, Paper, Text } from "@mantine/core";
+import GoogleIcon from "@components/auth/icons/google-icon";
+import GithubIcon from "@components/auth/icons/github-icon";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (session?.user?.email) {
+    return {
+      redirect: {
+        destination: "/tracking",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
+
+const SignIn = () => {
+  return (
+    <Layout>
+      <div className="flex flex-1 justify-center">
+        <Paper radius="md" p="xl" withBorder className="m-3 self-center">
+          <Text size="lg" weight={500}>
+            Welcome to Budget IO!
+          </Text>
+          <Group mb="md" mt="md">
+            <Button
+              leftIcon={<GithubIcon />}
+              onClick={() => signIn("github")}
+              className="bg-zinc-800 text-white hover:bg-zinc-700"
+            >
+              Continue with GitHub
+            </Button>
+            <Button
+              leftIcon={<GoogleIcon />}
+              variant="default"
+              color="gray"
+              onClick={() => signIn("google")}
+            >
+              Continue with Google
+            </Button>
+          </Group>
+        </Paper>
+      </div>
+    </Layout>
+  );
+};
+
+export default SignIn;
