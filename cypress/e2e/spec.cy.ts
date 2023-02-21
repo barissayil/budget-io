6f31c881b2ccfffa16ad5d0a8de88e33e39f22d5
Baris Sayil <barissayil@protonmail.com>
2023-02-21 06:37:13 +0100

    🔧 cypress: baseUrl

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("http://localhost:3000/").contains("See features").click();
+    cy.visit("/").contains("See features").click();
     cy.contains("Get started").scrollIntoView().click();
     cy.url().should("include", "/auth/signin");
   });
