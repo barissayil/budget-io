0882595e212f836553245669bbabbbcca9ba124d
Baris Sayil <barissayil@protonmail.com>
2023-02-27 21:11:20 +0100

    ✅ e2e: tracking page

---
+++ cypress/e2e/tracking-page.cy.ts
@@ -0,0 +1,240 @@
+import dayjs from "dayjs";
+
+before(() => {
+  cy.register().login();
+});
+describe("testing the tracking page", { testIsolation: false }, () => {
+  const today = dayjs().format().substring(0, 10);
+  const firstOfPreviousMonth =
+    dayjs().subtract(1, "month").format().substring(0, 10).slice(0, 8) + "01";
+  const firstOfCurrentMonth = today.slice(0, 8) + "01";
+  const fifthOfCurrentMonth = today.slice(0, 8) + "05";
+  const tenthOfCurrentMonth = today.slice(0, 8) + "10";
+  const eleventhOfCurrentMonth = today.slice(0, 8) + "11";
+
+  it("should have no transactions in the current month", () => {
+    cy.visit("/").contains("No records").should("be.visible");
+  });
+
+  it("should be able to add a transaction", () => {
+    cy.addTransaction(5, 30, "Food", "Restaurant", "Restaurant X", false);
+    cy.contains("No records").should("not.be.visible");
+    cy.checkTransactionTable([
+      {
+        date: fifthOfCurrentMonth,
+        amount: 30,
+        category: "Food",
+        subcategory: "Restaurant",
+        details: "Restaurant X",
+      },
+    ]);
+  });
+
+  it("should be able to do add more transactions", () => {
+    cy.addTransaction(1, 1000, "Housing", "Rent", "Company X", false);
+    cy.addTransaction(11, 25.11, "Housing", "Hostel", "Hostel X", false);
+    cy.addTransaction(10, 9.99, "Food", "Groceries", "Market X", false);
+    cy.checkTransactionTable([
+      {
+        date: firstOfCurrentMonth,
+        amount: 1000,
+        category: "Housing",
+        subcategory: "Rent",
+        details: "Company X",
+      },
+      {
+        date: fifthOfCurrentMonth,
+        amount: 30,
+        category: "Food",
+        subcategory: "Restaurant",
+        details: "Restaurant X",
+      },
+      {
+        date: tenthOfCurrentMonth,
+        amount: 9.99,
+        category: "Food",
+        subcategory: "Groceries",
+        details: "Market X",
+      },
+      {
+        date: eleventhOfCurrentMonth,
+        amount: 25.11,
+        category: "Housing",
+        subcategory: "Hostel",
+        details: "Hostel X",
+      },
+    ]);
+  });
+
+  it("should be able to edit a transaction", () => {
+    cy.intercept({
+      method: "PUT",
+      url: "/api/transaction/*",
+    }).as("updateTransaction");
+
+    cy.get(".hidden .icon-tabler-edit").first().click();
+    cy.get("form")
+      .should("be.visible")
+      .within(() => {
+        cy.get('input[placeholder*="Amount"]').clear().type("900");
+        cy.get('input[placeholder*="Details"]')
+          .clear()
+          .type("Company Y")
+          .type("{downarrow}")
+          .type("{enter}");
+        cy.contains("Update").click();
+      });
+
+    cy.contains("Editing").should("be.visible");
+    cy.contains("The transaction is being edited.").should("be.visible");
+    cy.wait("@updateTransaction", { timeout: 10000 });
+    cy.contains("Edited").should("be.visible");
+    cy.contains("The transaction is edited.").should("be.visible");
+
+    cy.checkTransactionTable([
+      {
+        date: firstOfCurrentMonth,
+        amount: 900,
+        category: "Housing",
+        subcategory: "Rent",
+        details: "Company Y",
+      },
+      {
+        date: fifthOfCurrentMonth,
+        amount: 30,
+        category: "Food",
+        subcategory: "Restaurant",
+        details: "Restaurant X",
+      },
+      {
+        date: tenthOfCurrentMonth,
+        amount: 9.99,
+        category: "Food",
+        subcategory: "Groceries",
+        details: "Market X",
+      },
+      {
+        date: eleventhOfCurrentMonth,
+        amount: 25.11,
+        category: "Housing",
+        subcategory: "Hostel",
+        details: "Hostel X",
+      },
+    ]);
+  });
+
+  it("should be able to delete a transaction", () => {
+    cy.intercept({
+      method: "DELETE",
+      url: "/api/transaction/*",
+    }).as("deleteTransaction");
+
+    cy.get(".hidden .icon-tabler-trash").eq(1).click();
+    cy.contains(/^Delete$/).click();
+
+    cy.contains("Deleting").should("be.visible");
+    cy.contains("The transaction is being deleted.").should("be.visible");
+    cy.wait("@deleteTransaction", { timeout: 10000 });
+    cy.contains("Deleted").should("be.visible");
+    cy.contains("The transaction is deleted.").should("be.visible");
+
+    cy.checkTransactionTable([
+      {
+        date: firstOfCurrentMonth,
+        amount: 900,
+        category: "Housing",
+        subcategory: "Rent",
+        details: "Company Y",
+      },
+      {
+        date: tenthOfCurrentMonth,
+        amount: 9.99,
+        category: "Food",
+        subcategory: "Groceries",
+        details: "Market X",
+      },
+      {
+        date: eleventhOfCurrentMonth,
+        amount: 25.11,
+        category: "Housing",
+        subcategory: "Hostel",
+        details: "Hostel X",
+      },
+    ]);
+  });
+
+  it("should be able to filter transactions by category", () => {
+    cy.get('input[placeholder*="Filter by category"]').click().type("{downarrow}").type("{enter}");
+    cy.checkTransactionTable([
+      {
+        date: tenthOfCurrentMonth,
+        amount: 9.99,
+        category: "Food",
+        subcategory: "Groceries",
+        details: "Market X",
+      },
+    ]);
+
+    cy.get('input[placeholder*="Filter by category"]').click().type("{downarrow}").type("{enter}");
+    cy.checkTransactionTable([
+      {
+        date: firstOfCurrentMonth,
+        amount: 900,
+        category: "Housing",
+        subcategory: "Rent",
+        details: "Company Y",
+      },
+      {
+        date: eleventhOfCurrentMonth,
+        amount: 25.11,
+        category: "Housing",
+        subcategory: "Hostel",
+        details: "Hostel X",
+      },
+    ]);
+
+    cy.get('input[placeholder*="Filter by category"]').click().clear();
+    cy.checkTransactionTable([
+      {
+        date: firstOfCurrentMonth,
+        amount: 900,
+        category: "Housing",
+        subcategory: "Rent",
+        details: "Company Y",
+      },
+      {
+        date: tenthOfCurrentMonth,
+        amount: 9.99,
+        category: "Food",
+        subcategory: "Groceries",
+        details: "Market X",
+      },
+      {
+        date: eleventhOfCurrentMonth,
+        amount: 25.11,
+        category: "Housing",
+        subcategory: "Hostel",
+        details: "Hostel X",
+      },
+    ]);
+  });
+
+  it("should have no transactions in the previous month", () => {
+    cy.get(".icon-tabler-arrow-left").click();
+    cy.contains("No records").should("be.visible");
+  });
+
+  it("should be able to add a transaction to the previous month", () => {
+    cy.addTransaction(1, 1250, "Housing", "Rent", "Company Z", true);
+    cy.contains("No records").should("not.be.visible");
+    cy.checkTransactionTable([
+      {
+        date: firstOfPreviousMonth,
+        amount: 1250,
+        category: "Housing",
+        subcategory: "Rent",
+        details: "Company Z",
+      },
+    ]);
+  });
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,11 +1,13 @@
 Cypress.Commands.add("register", () => {
   cy.log("register");
+
   cy.task("db:seed:user");
 });
 
 Cypress.Commands.add("login", () => {
-  const sessionToken = "04456e41-ec3b-4edf-92c1-48c14e57cacd2";
   cy.log("login");
+
+  const sessionToken = "04456e41-ec3b-4edf-92c1-48c14e57cacd2";
   cy.task("db:seed:session", sessionToken);
   cy.setCookie("next-auth.session-token", sessionToken, {
     domain: "localhost",
@@ -15,12 +17,126 @@ Cypress.Commands.add("login", () => {
   });
 });
 
+Cypress.Commands.add(
+  "addTransaction",
+  (
+    day: number,
+    amount: number,
+    category: string,
+    subcategory: string,
+    details: string,
+    previousMonth: boolean
+  ) => {
+    cy.log("add transaction");
+
+    cy.intercept({
+      method: "GET",
+      url: "/api/transaction/category",
+    }).as("getCategories");
+    cy.intercept({
+      method: "GET",
+      url: "/api/transaction/subcategory/*",
+    }).as("getSubcategories");
+    cy.intercept({
+      method: "GET",
+      url: "/api/transaction/details/*/*",
+    }).as("getDetails");
+    cy.intercept({
+      method: "POST",
+      url: "/api/transaction",
+    }).as("createTransaction");
+
+    cy.contains("Add transaction").click();
+
+    cy.get("form")
+      .should("be.visible")
+      .within(() => {
+        cy.get('input[placeholder*="Date"]').click();
+        if (previousMonth) cy.get(".mantine-DatePicker-calendarHeaderControl").first().click();
+        cy.contains(new RegExp("^" + String(day) + "$")).click();
+        cy.get('input[placeholder*="Amount"]').type(String(amount));
+        cy.wait("@getCategories", { timeout: 10000 })
+          .get('input[placeholder*="Category"]')
+          .type(category)
+          .type("{downarrow}")
+          .type("{enter}");
+        cy.wait("@getSubcategories", { timeout: 10000 })
+          .get('input[placeholder*="Subcategory"]')
+          .type(subcategory)
+          .type("{downarrow}")
+          .type("{enter}");
+        cy.wait("@getDetails", { timeout: 10000 })
+          .get('input[placeholder*="Details"]')
+          .type(details)
+          .type("{downarrow}")
+          .type("{enter}");
+        cy.contains("Add").click();
+      });
+
+    cy.contains("Adding").should("be.visible");
+    cy.contains("The transaction is being added.").should("be.visible");
+    cy.wait("@createTransaction", { timeout: 10000 });
+    cy.contains("Added").should("be.visible");
+    cy.contains("The transaction is added.").should("be.visible");
+  }
+);
+
+Cypress.Commands.add(
+  "checkTransactionTable",
+  (
+    transactions: {
+      date: string;
+      amount: number;
+      category: string;
+      subcategory: string;
+      details: string;
+    }[]
+  ) => {
+    cy.log("check transaction table");
+
+    cy.get(".hidden tbody").children().as("rows");
+    transactions.forEach(({ date, amount, category, subcategory, details }, i) => {
+      cy.get("@rows")
+        .eq(i)
+        .children()
+        .first()
+        .contains(date)
+        .next()
+        .contains(new RegExp("^" + String(amount) + "$"))
+        .next()
+        .contains(category)
+        .next()
+        .contains(subcategory)
+        .next()
+        .contains(details);
+    });
+    cy.get("@rows").eq(transactions.length).should("not.exist");
+  }
+);
+
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       register(): Chainable<void>;
       login(): Chainable<void>;
+      addTransaction(
+        day: number,
+        amount: number,
+        category: string,
+        subcategory: string,
+        details: string,
+        previousMonth: boolean
+      ): Chainable<void>;
+      checkTransactionTable(
+        transactions: {
+          date: string;
+          amount: number;
+          category: string;
+          subcategory: string;
+          details: string;
+        }[]
+      ): Chainable<void>;
     }
   }
 }
