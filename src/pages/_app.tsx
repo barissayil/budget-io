609d841816ed7ff341ff70b0ea6678dcb24fcf44
Baris Sayil <barissayil@protonmail.com>
2023-02-28 15:04:39 +0100

    ♻️ import SWRConfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import { SessionProvider } from "next-auth/react";
-import { SWRConfig } from "swr/_internal";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
