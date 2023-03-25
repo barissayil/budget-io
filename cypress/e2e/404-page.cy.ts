caa520fa701aa24d41038b4f183b10dfaa36cc66
Baris Sayil <barissayil@protonmail.com>
2023-03-25 11:17:06 +0100

    ✅  404-page: all viewports

--- cypress/e2e/404-page.cy.ts
+++ cypress/e2e/404-page.cy.ts
@@ -1,35 +1,41 @@
+import { viewports } from "cypress/support/constants";
+
 describe("testing the 404 page", () => {
   const { baseUrl } = Cypress.config();
 
-  before(() => {
-    cy.register();
-  });
+  viewports.forEach((viewport) => {
+    describe(`with viewport ${viewport[0]}x${viewport[1]}`, () => {
+      before(() => {
+        cy.register();
+      });
 
-  it("should render the 404 page correctly", () => {
-    cy.visit("/invalid-url", { failOnStatusCode: false })
-      .url()
-      .should("equal", baseUrl + "/invalid-url");
-    cy.contains("You have found a secret place.").should("be.visible");
-    cy.contains(
-      "Unfortunately, this is only a 404 page." +
-        " You may have mistyped the address, or the page has been moved to another URL."
-    ).should("be.visible");
+      it("should render the 404 page correctly", () => {
+        cy.viewport(viewport[0], viewport[1]);
 
-    cy.contains("Take me back")
-      .click()
-      .url()
-      .should("equal", baseUrl + "/");
+        cy.visit("/invalid-url", { failOnStatusCode: false })
+          .url()
+          .should("equal", baseUrl + "/invalid-url");
+        cy.contains("You have found a secret place.").should("be.visible");
+        cy.contains(
+          "Unfortunately, this is only a 404 page." +
+            " You may have mistyped the address, or the page has been moved to another URL."
+        ).should("be.visible");
 
-    cy.login()
-      .visit("/invalid-url", { failOnStatusCode: false })
-      .url({ timeout: 60000 })
-      .should("equal", baseUrl + "/invalid-url");
+        cy.contains("Take me back")
+          .click()
+          .url()
+          .should("equal", baseUrl + "/");
 
-    cy.contains("Take me back")
-      .click()
-      .url({ timeout: 60000 })
-      .should("equal", baseUrl + "/tracking");
+        cy.login()
+          .visit("/invalid-url", { failOnStatusCode: false })
+          .url({ timeout: 60000 })
+          .should("equal", baseUrl + "/invalid-url");
+
+        cy.contains("Take me back")
+          .click()
+          .url({ timeout: 60000 })
+          .should("equal", baseUrl + "/tracking");
+      });
+    });
   });
 });
-
-export {};
